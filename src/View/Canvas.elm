2b54f455d83847398b4b53019d0f015745696c68
luizgaroland <luizgaroland@gmail.com>
2016-01-18 21:52:50 +0000

    Adding elm.js to git ignore

--- src/View/Canvas.elm
+++ src/View/Canvas.elm
@@ -24,53 +24,113 @@ mainFlowDown (w, h) game =
                 container w 100 middle
                     <| Html.toElement 240 100
                     <| h1 [] [ Html.text "Peg Leg Solitaire" ]
-                    
+             
             ,   container w 325 middle
                     <| Html.toElement 335 325
                     <| renderBoard game 
+
+            ,   container w 80 middle
+                    <| Html.toElement 90 50
+                    <| h2 [] [ Html.text "Controls" ]
+                    
+            ,   container w 50 middle
+                    <| Html.toElement 121 50
+                    <| p []
+                        [ 
+                            Html.text 
+                                "Space - Make Play" 
+                        ]
+                        
+            ,   container w 50 middle
+                    <| Html.toElement 303 50
+                    <| p []
+                        [ 
+                            Html.text 
+                                "Arrows - Move Cursor | Choose Play Direction" 
+                        ]
             ]            
     
     else if game.gameState == Win then
         flow down
-                [
-                    container w 100 middle
-                        <| Html.toElement 240 100
-                        <| h1 [] [ Html.text "Peg Leg Solitaire" ]
+            [
+                container w 100 middle
+                    <| Html.toElement 240 100
+                    <| h1 [] [ Html.text "Peg Leg Solitaire" ]
+                    
+            ,   container w 150 middle
+                    <| Html.toElement 505 80
+                    <| h1 [] [ 
+                                Html.text 
+                                    "Huzzah! You Win, Congratulations!" 
+                             ]
+                             
+            ,   container w 150 middle
+                    <| Html.toElement 185 80
+                    <| h4 [] [ 
+                                Html.text 
+                                    "Press Space to Play Again!" 
+                             ]
+                             
+            ,   container w 80 middle
+                <| Html.toElement 90 50
+                <| h2 [] [ Html.text "Controls" ]
+                
+            ,   container w 50 middle
+                    <| Html.toElement 121 50
+                    <| p []
+                        [ 
+                            Html.text 
+                                "Space - Make Play" 
+                        ]
                         
-                ,   container w 150 middle
-                        <| Html.toElement 505 80
-                        <| h1 [] [ 
-                                    Html.text 
-                                        "Huzzah! You Win, Congratulations!" 
-                                 ]
-                                 
-                ,   container w 150 middle
-                        <| Html.toElement 185 80
-                        <| h4 [] [ 
-                                    Html.text 
-                                        "Press Space to Play Again!" 
-                                 ]
-                ]
+            ,   container w 50 middle
+                    <| Html.toElement 303 50
+                    <| p []
+                        [ 
+                            Html.text 
+                                "Arrows - Move Cursor | Choose Play Direction" 
+                        ]
+            ]
     
     else if game.gameState == Loss then
         flow down
-                [
-                    container w 100 middle
-                        <| Html.toElement 240 100
-                        <| h1 [] [ Html.text "Peg Leg Solitaire" ]
+            [
+                container w 100 middle
+                    <| Html.toElement 240 100
+                    <| h1 [] [ Html.text "Peg Leg Solitaire" ]
+                    
+            ,   container w 150 middle
+                    <| Html.toElement 355 105
+                    <| h1 [] [ Html.text "You lost! Sucks to be you!" ]
+                    
+            ,   container w 150 middle
+                    <| Html.toElement 185 80
+                    <| h4 [] [ 
+                                Html.text 
+                                    "Press Space to Play Again!" 
+                             ]       
+
+            ,   container w 80 middle
+                <| Html.toElement 90 50
+                <| h2 [] [ Html.text "Controls" ]
+                
+            ,   container w 50 middle
+                    <| Html.toElement 121 50
+                    <| p []
+                        [ 
+                            Html.text 
+                                "Space - Make Play" 
+                        ]
                         
-                ,   container w 150 middle
-                        <| Html.toElement 365 105
-                        <| h1 [] [ Html.text "You lost! Sucks to be you!" ]
-                        
-                 ,   container w 150 middle
-                        <| Html.toElement 185 80
-                        <| h4 [] [ 
-                                    Html.text 
-                                        "Press Space to Play Again!" 
-                                 ]       
-                ]
-    
+            ,   container w 50 middle
+                    <| Html.toElement 303 50
+                    <| p []
+                        [ 
+                            Html.text 
+                                "Arrows - Move Cursor | Choose Play Direction" 
+                        ]
+            ]
+
     else
         flow down
             [
